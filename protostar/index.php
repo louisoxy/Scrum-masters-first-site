f08a213abeee882c6c21280cb4a0e81d7df46f11
louisoxy <oxyfit@outlook.com>
2017-11-06 14:04:17 +0000

    Add files via upload

---
+++ protostar/index.php
@@ -0,0 +1,345 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  Templates.protostar
+ *
+ * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+/** @var JDocumentHtml $this */
+
+$app  = JFactory::getApplication();
+$user = JFactory::getUser();
+
+// Output as HTML5
+$this->setHtml5(true);
+
+// Getting params from template
+$params = $app->getTemplate(true)->params;
+
+// Detecting Active Variables
+$option   = $app->input->getCmd('option', '');
+$view     = $app->input->getCmd('view', '');
+$layout   = $app->input->getCmd('layout', '');
+$task     = $app->input->getCmd('task', '');
+$itemid   = $app->input->getCmd('Itemid', '');
+$sitename = $app->get('sitename');
+
+if ($task === 'edit' || $layout === 'form')
+{
+	$fullWidth = 1;
+}
+else
+{
+	$fullWidth = 0;
+}
+
+// Add JavaScript Frameworks
+JHtml::_('bootstrap.framework');
+
+// Add template js
+JHtml::_('script', 'template.js', array('version' => 'auto', 'relative' => true));
+
+// Add html5 shiv
+JHtml::_('script', 'jui/html5.js', array('version' => 'auto', 'relative' => true, 'conditional' => 'lt IE 9'));
+
+// Add Stylesheets
+JHtml::_('stylesheet', 'template.css', array('version' => 'auto', 'relative' => true));
+
+// Use of Google Font
+if ($this->params->get('googleFont'))
+{
+	JHtml::_('stylesheet', '//fonts.googleapis.com/css?family=' . $this->params->get('googleFontName'));
+	$this->addStyleDeclaration("
+	h1, h2, h3, h4, h5, h6, .site-title {
+		font-family: '" . str_replace('+', ' ', $this->params->get('googleFontName')) . "', sans-serif;
+	}");
+}
+
+// Template color
+if ($this->params->get('templateColor'))
+{
+	$this->addStyleDeclaration('
+	body.site {
+		border-top: 3px solid ' . $this->params->get('templateColor') . ';
+		background-color: ' . $this->params->get('templateBackgroundColor') . ';
+	}
+	a {
+		color: ' . $this->params->get('templateColor') . ';
+	}
+	.nav-list > .active > a,
+	.nav-list > .active > a:hover,
+	.dropdown-menu li > a:hover,
+	.dropdown-menu .active > a,
+	.dropdown-menu .active > a:hover,
+	.nav-pills > .active > a,
+	.nav-pills > .active > a:hover,
+	.btn-primary {
+		background: ' . $this->params->get('templateColor') . ';
+	}');
+}
+
+// Check for a custom CSS file
+JHtml::_('stylesheet', 'user.css', array('version' => 'auto', 'relative' => true));
+
+// Check for a custom js file
+JHtml::_('script', 'user.js', array('version' => 'auto', 'relative' => true));
+
+// Load optional RTL Bootstrap CSS
+JHtml::_('bootstrap.loadCss', false, $this->direction);
+
+// Adjusting content width
+$position7ModuleCount = $this->countModules('position-7');
+$position8ModuleCount = $this->countModules('position-8');
+
+if ($position7ModuleCount && $position8ModuleCount)
+{
+	$span = 'span6';
+}
+elseif ($position7ModuleCount && !$position8ModuleCount)
+{
+	$span = 'span9';
+}
+elseif (!$position7ModuleCount && $position8ModuleCount)
+{
+	$span = 'span9';
+}
+else
+{
+	$span = 'span12';
+}
+
+// Logo file or site title param
+if ($this->params->get('logoFile'))
+{
+	$logo = '<img src="' . JUri::root() . $this->params->get('logoFile') . '" alt="' . $sitename . '" />';
+}
+elseif ($this->params->get('sitetitle'))
+{
+	$logo = '<span class="site-title" title="' . $sitename . '">' . htmlspecialchars($this->params->get('sitetitle'), ENT_COMPAT, 'UTF-8') . '</span>';
+}
+else
+{
+	$logo = '<span class="site-title" title="' . $sitename . '">' . $sitename . '</span>';
+}
+?>
+<!DOCTYPE html>
+<head>
+  <!-- Theme Made By www.w3schools.com - No Copyright -->
+  <title>Bootstrap Theme Company Page</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+  #top-header{
+	  text-align:right;
+  }
+  .navbar navbar-default navbar-fixed-top{
+	  background-color:white;
+  }
+  
+  .jumbotron {
+      background-color: #a87bb8;
+      color: #fff;
+      padding: 100px 25px;
+	  margin-top:110px;
+  }
+  .container-fluid {
+      padding: 60px 50px;
+  }
+  .bg-grey {
+      background-color: #f6f6f6;
+  }
+  .logo-small {
+      color: #a87bb8;
+      font-size: 50px;
+  }
+  .logo {
+      color: #a87bb8;
+      font-size: 200px;
+  }
+  .thumbnail {
+      padding: 0 0 15px 0;
+      border: none;
+      border-radius: 0;
+  }
+  .thumbnail img {
+      width: 100%;
+      height: 100%;
+      margin-bottom: 10px;
+  }
+  .carousel-control.right, .carousel-control.left {
+      background-image: none;
+      color: #f4511e;
+  }
+  .carousel-indicators li {
+      border-color: #f4511e;
+  }
+  .carousel-indicators li.active {
+      background-color: #f4511e;
+  }
+  .item h4 {
+      font-size: 19px;
+      line-height: 1.375em;
+      font-weight: 400;
+      font-style: italic;
+      margin: 70px 0;
+  }
+  .item span {
+      font-style: normal;
+  }
+  .panel {
+      border: 1px solid #a87bb8; 
+      border-radius:0 !important;
+      transition: box-shadow 0.5s;
+  }
+  .panel:hover {
+      box-shadow: 5px 0px 40px rgba(0,0,0, .2);
+  }
+  .panel-footer .btn:hover {
+      border: 1px solid #a87bb8;
+      background-color: #fff !important;
+      color: #f4511e;
+  }
+  .panel-heading {
+      color: #fff !important;
+      background-color: #a87bb8 !important;
+      padding: 25px;
+      border-bottom: 1px solid transparent;
+      border-top-left-radius: 0px;
+      border-top-right-radius: 0px;
+      border-bottom-left-radius: 0px;
+      border-bottom-right-radius: 0px;
+  }
+  .panel-footer {
+      background-color: white !important;
+  }
+  .panel-footer h3 {
+      font-size: 32px;
+  }
+  .panel-footer h4 {
+      color: #a87bb8;
+      font-size: 14px;
+  }
+  .panel-footer .btn {
+      margin: 15px 0;
+      background-color: #f4511e;
+      color: #fff;
+  }
+  @media screen and (max-width: 768px) {
+    .col-sm-4 {
+      text-align: center;
+      margin: 25px 0;
+    }
+  }
+  #main{
+	margin-top:100;
+  }
+  
+  #footer{
+	  background-color:#36454f;
+  }
+  
+  p{
+	  color: white;
+	  
+  }
+  </style>
+</head>
+<body>
+
+<nav class="navbar navbar-default navbar-fixed-top">
+
+	<div id="top-header" class="row">
+		<div class="col-md-12">
+			<h3>Customer Log-in / Register</h3><br><h1>0161 7054382</h1>
+		</div>
+	</div>
+
+	  <div class="container">
+	  <div class="row">
+			<div class="col-md-12">
+			<div class="navbar-header">
+			  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>                        
+			  </button>
+			  <a class="navbar-brand" href="#myPage">Logo</a>
+			</div>
+			
+			<div class="collapse navbar-collapse" id="myNavbar">
+			  <ul class="nav navbar-nav navbar-right">
+			  
+				<li><a href="#about">Will Writing</a></li>
+				<li><a href="#services">Trust Information</a></li>
+				<li><a href="#portfolio">Power of Attorney</a></li>
+				<li><a href="#pricing">Funeral Plans</a></li>
+				<li><a href="#contact">Business Wills</a></li>
+				<li><a href="#pricing">Probate</a></li>
+				<li><a href="#contact">Contact us</a></li>
+			  </ul>
+			</div>
+		  </div>
+	  </div>
+  </div>
+</nav>
+
+ <div id="main" class="row">
+    <div class="col-md-9">
+<div class="jumbotron text-center">
+  <h1>THE WILL WRITING PARTNERSHIP</h1> 
+  <p>We specialize in wills</p> 
+  <form class="form-inline">
+    <div class="input-group">
+     
+      <div class="input-group-btn">
+        <button type="button" class="btn btn-danger">Find Out More</button>
+      </div>
+    </div>
+  </form>
+</div>
+</div>
+</div>
+
+<!-- Container (About Section) -->
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-sm-12">
+      <div class="collapse navbar-collapse" id="myNavbar">
+			  <ul class="nav navbar-nav navbar-right">
+			  
+				<li><a href="#about">Private Document Storage</a></li>
+				<li><a href="#services">News Blog</a></li>
+				<li><a href="#portfolio">About</a></li>
+				<li><a href="#pricing">Explanations and Definitions</a></li>
+				<li><a href="#contact">T&Cs / Privacy / Site plan / Association logos / quick link nav / contact us</a></li>
+				
+			  </ul>
+			</div>
+  </div>
+</div>
+</div>
+
+
+<!-- Container (Contact Section) -->
+
+
+<footer id="footer" class="container-fluid text-center">
+  <a href="#myPage" title="To Top">
+    <span class="glyphicon glyphicon-chevron-up"></span>
+  </a>
+  <p>Will Writhing Partnership<p>
+</footer>
+<!--
+To use this code on your website, get a free API key from Google.
+Read more at: https://www.w3schools.com/graphics/google_maps_basic.asp
+-->
+
+</body>
+</html>
