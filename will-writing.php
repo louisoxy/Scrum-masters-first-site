f5e60ed136ec0dba3cc93a8cebf06d45a488d1b6
louisoxy <oxyfit@outlook.com>
2017-11-17 18:33:42 +0000

    Add files via upload

---
+++ will-writing.php
@@ -0,0 +1,74 @@
+<?php /* Template Name: WillWriting */ ?>
+<link href="<?php echo get_bloginfo('template_directory'); ?>/blog.css" rel="stylesheet">
+<?php get_header(); ?>
+
+	
+<div class="container-fluid">
+	   <div class="row">
+			<div class="col-md-9">
+				<img src="//placehold.it/1624x400" class="img-responsive">
+		   </div>
+		   <div class="panel-group">
+		   <div class="col-md-3">
+				<div class="panel panel-primary">
+					<div class="panel-heading">Promo Add</div>
+					<div class="panel-body">Content</div>
+				</div>
+			</div>
+			</DIV>
+			 <div class="panel-group">
+				<div class="col-md-3">
+					<div class="panel panel-primary">
+						<div class="panel-heading">Promo Add</div>
+						<div class="panel-body">Content</div>
+					</div>
+				</div>
+			</DIV>
+	   </div>
+</div>
+
+<div class="centered">
+<div class="tabs">
+<ul class="nav nav-tabs">
+    <li class="active"><a data-toggle="tab" href="#home">Your Will – The Basics</a></li>
+    <li><a data-toggle="tab" href="#menu1">You can Make A Will Yourself</a></li>
+    <li><a data-toggle="tab" href="#menu2">Step-by-step Guide</a></li>
+    <li><a data-toggle="tab" href="#menu3">FAQs</a></li>
+  </ul>
+
+  <div class="tab-content">
+    <div id="home" class="tab-pane fade in active">
+      <h3>Your Will – The Basics </h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+    <div id="menu1" class="tab-pane fade">
+      <h3>You can Make A Will Yourself</h3>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+    <div id="menu2" class="tab-pane fade">
+      <h3>Step-by-step Guide</h3>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+    </div>
+    <div id="menu3" class="tab-pane fade">
+      <h3>FAQs</h3>
+      <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="container-fluid">
+    
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Will writing</a></li>
+     
+     <li><a href="#">Private Document Storage</a></li>
+          <li><a href="#">News blog</a></li>
+          <li><a href="#">About</a></li>
+		   <li><a href="#">Explanations and Definitions</a></li>
+          <li><a href="#">T&Cs / Privacy / Site plan / Asssociation logos / quick link nav / contact us</a></li>
+    </ul>
+  </div>
+ 
+
+<?php get_footer(); ?>
