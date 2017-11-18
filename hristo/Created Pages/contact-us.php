ad121cae0970b0e126b7c3fed405a38e3cb4fb23
Hristo <hsp1amt@bolton.ac.uk>
2017-11-18 14:05:27 +0000

    Add files via upload

---
+++ hristo/Created Pages/contact-us.php
@@ -0,0 +1,71 @@
+<?php /* Template Name: Probate */ ?>
+<link href="<?php echo get_bloginfo('template_directory'); ?>/blog.css" rel="stylesheet">
+<?php get_header(); ?>
+
+
+	<div class="container-fluid">
+	   <div class="row">
+			<div class="col-md-9">
+				<div class="centered">
+<div class="col-md-8 col-md-offset-2">
+<h3>Contact info</h3>
+<hr>
+<h4>Or you can contact us directly by filling your details bellow:</h4>
+                  <form class="form-horizontal">
+                    <div class="form-group">
+                      <label for="exampleInputName2">Name</label>
+                      <input type="text" class="form-control" id="exampleInputName2" placeholder="Your Full Name">
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleInputEmail2">Email</label>
+                      <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Your Email">
+                    </div>
+                    <div class="form-group ">
+                      <label for="exampleInputText">Your Message</label>
+                     <textarea  class="form-control" rows="20" placeholder="Your message to us"></textarea> 
+                    </div>
+                    <button type="submit" class="btn btn-default">Send Message</button>
+                  </form>
+</div>
+ 
+  
+ 
+  
+</div>
+		   </div>
+		   <div class="panel-group">
+		   <div class="col-md-3">
+				<div class="panel panel-primary">
+					<div class="panel-heading">Promo Ad</div>
+					<div class="panel-body">Content</div>
+				</div>
+			</div>
+			</DIV>
+			 <div class="panel-group">
+				<div class="col-md-3">
+					<div class="panel panel-primary">
+						<div class="panel-heading">Promo Ad</div>
+						<div class="panel-body">Content</div>
+					</div>
+				</div>
+			</DIV>
+	   </div>
+</div>
+
+</div>
+<div class="container-fluid">
+     <ul class="nav navbar-nav">
+   
+ 
+      
+     
+     <li><a href="">Private Document Storage</a></li>
+          <li><a href="#">News blog</a></li>
+          <li><a href="#">About</a></li>
+		   <li><a href="#">Explanations and Definitions</a></li>
+          <li><a href="#">T&Cs / Privacy / Site plan / Asssociation logos / quick link nav / contact us</a></li>
+    </ul>
+  </div>
+  </div>
+
+<?php get_footer(); ?>
---
+++ hristo/Created Pages/probate.php
@@ -0,0 +1,62 @@
+<?php /* Template Name: Probate */ ?>
+<link href="<?php echo get_bloginfo('template_directory'); ?>/blog.css" rel="stylesheet">
+<?php get_header(); ?>
+
+	
+<div class="container-fluid">
+	   <div class="row">
+			<div class="col-md-9">
+				<img src="//placehold.it/1624x400" class="img-responsive">
+		   </div>
+		   <div class="panel-group">
+		   <div class="col-md-3">
+				<div class="panel panel-primary">
+					<div class="panel-heading">Promo Ad</div>
+					<div class="panel-body">Content</div>
+				</div>
+			</div>
+			</DIV>
+			 <div class="panel-group">
+				<div class="col-md-3">
+					<div class="panel panel-primary">
+						<div class="panel-heading">Promo Ad</div>
+						<div class="panel-body">Content</div>
+					</div>
+				</div>
+			</DIV>
+	   </div>
+</div>
+<div class="centered">
+<div class="tabs">
+<ul class="nav nav-tabs">
+    <li><a data-toggle="tab" href="#home">FAQs</a></li>
+    
+  </ul>
+
+  <div class="tab-content">
+    <div id="home" class="tab-pane fade">
+      <h3>Frequently asked questions </h3>
+	  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam a risus eget turpis vulputate porta in ac tellus. Suspendisse dignissim porta urna, ac tincidunt nisi pulvinar sed. Aliquam vitae auctor erat.</p>
+
+    </div>
+ 
+  
+ 
+  
+</div>
+</div>
+<div class="container-fluid">
+    
+    <ul class="nav navbar-nav">
+      
+     
+     <li><a href="will-writing.php">Private Document Storage</a></li>
+          <li><a href="#">News blog</a></li>
+          <li><a href="#">About</a></li>
+		   <li><a href="#">Explanations and Definitions</a></li>
+          <li><a href="#">T&Cs / Privacy / Site plan / Asssociation logos / quick link nav / contact us</a></li>
+    </ul>
+  </div>
+  </div>
+
+<?php get_footer(); ?>
